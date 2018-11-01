d5a7c21b4d8a12be5c6bf1e1dd2bb00422f7ccbd
adikantawala <adikan@ucla.edu>
2018-11-01 15:39:29 -0700

    Finish #second_anagram?

--- anagrams.rb
+++ anagrams.rb
@@ -1,2 +1,17 @@
 
-def first_anagram(str)
+def first_anagram(str1,str2)
+  str1.chars.permutation.to_a.include?(str2.chars)
+end
+
+def second_anagram?(str1,str2)
+  str1.chars.each do |ch|
+    if str2.include?(ch)
+      idx = str2.index(ch)
+      str2[idx] = ""
+    else
+      return false
+    end
+
+  end
+  str2 == "" ? true : false
+end
